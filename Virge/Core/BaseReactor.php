d9bcc541739261cf60cc989c098c4fb4f1ff6920
Kramer <kramer@siosphere.com>
2015-09-10 01:20:05 -0700

    Enabled caching

--- Virge/Core/BaseReactor.php
+++ Virge/Core/BaseReactor.php
@@ -2,6 +2,7 @@
 
 namespace Virge\Core;
 
+use Virge\Core\Config;
 use Virge\Virge;
 
 /**
@@ -17,19 +18,36 @@ abstract class BaseReactor {
      * @param array $capsules
      */
     public function registerCapsules($capsules = array()) {
-        //todo: check cache
+        
+        $cachePath = Config::get('base_path') . 'storage/cache/';
+        if(is_file($cachePath . 'reactor.cache.php')) {
+            require_once $cachePath . 'reactor.cache.php';
+            $cached = true;
+        } else {
+            $cached = false;
+            $toCache = '';
+        }
+        
         foreach($capsules as $capsule) {
             //todo: load anything we need to, configs, etc
             $this->_capsules[] = $capsule;
             
-            $reflector = new \ReflectionClass(get_class($capsule));
-            $capsuleDir = dirname($reflector->getFileName()) . '/config/';
-            $capsuleArray = Virge::dirToArray($capsuleDir);
-            //crawl the config directory if it exists
-            $files = $capsuleArray ? $capsuleArray['file'] : array();
-            foreach($files as $file) {
-                require_once $capsuleDir . $file;
+            if(!$cached) {
+                $reflector = new \ReflectionClass(get_class($capsule));
+                $capsuleDir = dirname($reflector->getFileName()) . '/config/';
+                $capsuleArray = Virge::dirToArray($capsuleDir);
+                //crawl the config directory if it exists
+                $files = $capsuleArray ? $capsuleArray['file'] : array();
+                foreach($files as $file) {
+                    $toCache = file_get_contents($capsuleDir . $file) . "\n";
+                    require_once $capsuleDir . $file;
+                }
             }
+        }
+        
+        if(!$cached && Config::get('environment') === 'production') {
+            //save cache
+            file_put_contents($cachePath . 'reactor.cache.php', $toCache);
         }
     }
     
