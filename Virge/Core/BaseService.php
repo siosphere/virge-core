83abbf1417597bafaaed9c9123a404d413e2cff7
Michael Kramer <kramer@localhost.localdomain>
2016-10-01 15:34:55 -0700

    Instance services

--- Virge/Core/BaseService.php
+++ Virge/Core/BaseService.php
@@ -9,8 +9,28 @@ use Virge\Virge;
  */
 abstract class BaseService implements ServiceInterface
 {
+    public static function instance()
+    {
+        $className = static::class;
+
+        $service = Virge::service($className);
+        if($service) {
+            return $service;
+        }
+        $service = new $className(...static::_instanceProps());
+
+        Virge::registerService($className, $service);
+
+        return $service;
+    }
+
     protected static function service($serviceId)
     {
         return Virge::service($serviceId);
+    }
+
+    protected static function _instanceProps()
+    {
+        return [];
     }
 }
